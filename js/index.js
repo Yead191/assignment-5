1426ef66c551d7db9a23e6bf2605b64470817110
Yead <yead19@gmail.com>
2024-09-24 01:18:42 +0600

    added modal

--- js/index.js
+++ js/index.js
@@ -15,6 +15,19 @@ noakhaliBtn.addEventListener('click', function () {
     const wallet = parseFloat(document.getElementById('balance').innerText)
     const balance = document.getElementById('balance')
 
+    // validation
+
+    if (noakhaliInput <=0 || isNaN(noakhaliInput)) {
+        document.getElementById('noakhali-error').classList.remove('hidden')
+        return
+
+    }
+    else{
+        document.getElementById('noakhali-error').classList.add('hidden')
+    }
+   
+
+
     const newBalance = wallet - noakhaliInput;
     balance.innerText = newBalance
 
@@ -32,11 +45,12 @@ noakhaliBtn.addEventListener('click', function () {
     const historytab = document.createElement('div')
     historytab.classList.add('rounded-2xl', 'border', 'border-gray-200', "p-8", 'mb-8')
     historytab.innerHTML = `
-    <p class= "text-2xl font-bold mb-4">${noakhaliInput} Taka is Donated for Flood Relief in Noakhali,Bangladesh</p>
-    <p class="font-light text-md" >Date: ${new Date() }
+    <p class= "text-xl lg:text-2xl font-bold mb-4">${noakhaliInput} Taka is Donated for Flood Relief in Noakhali, Bangladesh</p>
+    <p class="font-light text-sm md:text-md" >Date: ${new Date()}
     `
     const historyContainer = document.getElementById('historyTab')
     historyContainer.insertBefore(historytab, historyContainer.firstChild)
+
 
 
 
@@ -52,6 +66,17 @@ laxBtn.addEventListener('click', function () {
     const inputlax = document.getElementById('laxInput')
     const wallet = parseFloat(document.getElementById('balance').innerText)
     const balance = document.getElementById('balance')
+
+    // validation
+
+    if (laxInput <=0 || isNaN(laxInput)) {
+        document.getElementById('lax-error').classList.remove('hidden')
+        return
+
+    }
+    else{
+        document.getElementById('lax-error').classList.add('hidden')
+    }
 
     const newBalance = wallet - laxInput;
     balance.innerText = newBalance
@@ -70,8 +95,8 @@ laxBtn.addEventListener('click', function () {
     const historytab = document.createElement('div')
     historytab.classList.add('rounded-2xl', 'border', 'border-gray-200', "p-8", "mb-8")
     historytab.innerHTML = `
-    <p class= "text-2xl font-bold mb-4">${laxInput} Taka is Donated for Flood Relief in Lakshmipur,Bangladesh</p>
-    <p class="font-light text-md" >Date: ${new Date() }
+    <p class= "text-xl lg:text-2xl font-bold mb-4">${laxInput} Taka is Donated for Flood Relief in Lakshmipur,Bangladesh</p>
+    <p class="font-light text-sm md:text-md" >Date: ${new Date()}
     `
     const historyContainer = document.getElementById('historyTab')
     historyContainer.insertBefore(historytab, historyContainer.firstChild)
@@ -88,6 +113,17 @@ quotaBtn.addEventListener('click', function () {
     const inputquota = document.getElementById('quotaInput')
     const wallet = parseFloat(document.getElementById('balance').innerText)
     const balance = document.getElementById('balance')
+    
+    // validation
+
+    if (quotaInput <=0 || isNaN(quotaInput)) {
+        document.getElementById('quota-error').classList.remove('hidden')
+        return
+
+    }
+    else{
+        document.getElementById('quota-error').classList.add('hidden')
+    }
 
     const newBalance = wallet - quotaInput;
     balance.innerText = newBalance
@@ -107,13 +143,47 @@ quotaBtn.addEventListener('click', function () {
     const historytab = document.createElement('div')
     historytab.classList.add('rounded-2xl', 'border', 'border-gray-200', "p-8", "mb-8")
     historytab.innerHTML = `
-    <p class= "text-2xl font-bold mb-4">${quotaInput} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</p>
-    <p class="font-light text-md" >Date: ${new Date() }
+    <p class= "w-full text-xl lg:text-2xl font-bold mb-4">${quotaInput} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</p>
+    <p class="w-full font-light text-sm md:text-md" >Date: ${new Date()}
     `
     const historyContainer = document.getElementById('historyTab')
     historyContainer.insertBefore(historytab, historyContainer.firstChild)
-    
+
 
 
 
 })
+
+// history Tab
+const historyMenu = document.getElementById('historyMenu')
+const donationMenu = document.getElementById('donationMenu')
+historyMenu.addEventListener('click', function () {
+    historyMenu.classList.add('bg-[#B4F461]')
+    historyMenu.classList.remove('bg-white')
+    donationMenu.classList.remove('bg-[#B4F461]')
+    donationMenu.classList.add('bg-white')
+
+    document.getElementById('donationSection').classList.add('hidden')
+    document.getElementById('historySection').classList.remove('hidden')
+
+
+
+})
+
+//  dontation tab
+
+donationMenu.addEventListener('click', function () {
+    donationMenu.classList.add('bg-[#B4F461]')
+    donationMenu.classList.remove('bg-white')
+    historyMenu.classList.remove('bg-[#B4F461]')
+    historyMenu.classList.add('bg-white')
+
+    document.getElementById('historySection').classList.add('hidden')
+    document.getElementById('donationSection').classList.remove('hidden')
+
+
+
+})
+
+
+
